95f40ad22a128eb8a110a3c97a0e9f81004c3cd1
Nuno Pereira <nunoafonso2002@gmail.com>
2024-08-04 20:56:46 +0100

    Added custom registration logic to account for promoter codes

---
+++ app/Http/Controllers/RegisteredUserController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Auth\StatefulGuard;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Str;
+use Laravel\Fortify\Contracts\CreatesNewUsers;
+use Laravel\Fortify\Contracts\RegisterResponse;
+use Laravel\Fortify\Contracts\RegisterViewResponse;
+use Laravel\Fortify\Fortify;
+
+class RegisteredUserController extends Controller
+{
+    /**
+     * The guard implementation.
+     *
+     * @var \Illuminate\Contracts\Auth\StatefulGuard
+     */
+    protected $guard;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(StatefulGuard $guard)
+    {
+        $this->guard = $guard;
+    }
+
+    /**
+     * Show the registration view.
+     */
+    public function create(Request $request): RegisterViewResponse
+    {
+        $promoter = null;
+        if ($request->is('register/promoter/*')) {
+            $promoter = $request->route('promoter');
+            dd($promoter);
+        }
+
+        return app(RegisterViewResponse::class);
+    }
+
+    /**
+     * Create a new registered user.
+     */
+    public function store(Request $request,
+        CreatesNewUsers $creator): RegisterResponse
+    {
+        if (config('fortify.lowercase_usernames')) {
+            $request->merge([
+                Fortify::username() => Str::lower($request->{Fortify::username()}),
+            ]);
+        }
+
+        event(new Registered($user = $creator->create($request->all())));
+
+        $this->guard->login($user);
+
+        return app(RegisterResponse::class);
+    }
+}
