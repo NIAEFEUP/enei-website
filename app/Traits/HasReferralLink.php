e3ca2cc29fdd036026e10e2521669429a520568e
AvilaAndre <avila_andre14@hotmail.com>
2024-08-25 23:45:36 +0100

    feat: store referral code cookie when on home page

--- app/Traits/HasReferralLink.php
+++ app/Traits/HasReferralLink.php
@@ -6,6 +6,12 @@ trait HasReferralLink
 {
     public function get_referral_link(): string
     {
+        // TODO: Add a proper referral code
+        $code = 'TODO'; // $this->promoter;
+        // FIXME: This code is currently using the home page
+        // for referrals, where should it redirect to?
+        $link = route('home', ['ref' => $code]);
+
         return 'TODO: implement this';
     }
 }
