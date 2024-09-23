e80254d28b292200950ef3b391ffccf55168d184
jose-carlos-sousa <139002032+jose-carlos-sousa@users.noreply.github.com>
2024-09-23 16:31:25 +0100

    update: improved error handling

--- app/Http/Controllers/QuestApiController.php
+++ app/Http/Controllers/QuestApiController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
 
 
 class QuestApiController extends Controller
@@ -17,8 +18,24 @@ class QuestApiController extends Controller
      */
     public function give(Request $request, Quest $quest): JsonResponse
     {
+        $validator = Validator::make($request->all(), [
+            'quest_code' => 'required|exists:participants,quest_code',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Invalid input',
+                'errors' => $validator->errors(),
+            ], 400);
+        }
         $editionId = $request->input('edition')->id;
+        if ($editionId === null) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Edition not found!',
+            ], 404);
+        }
         $participant = Participant::firstWhere('quest_code', $request->get('quest_code'));
 
         if (!$participant) {
@@ -26,7 +43,7 @@ class QuestApiController extends Controller
                 'status' => 'error',
                 'message' => 'Participant not found!',
             ], 404);
-        }
+        };
 
         $enrollment = $participant->enrollments()->where('edition_id', $editionId)->first();
         if ($enrollment === null) {
@@ -36,7 +53,18 @@ class QuestApiController extends Controller
             ], 400);
         }
 
-        $enrollment->quests()->attach($quest);
+        try {
+            $enrollment->quests()->attach($quest);
+        } catch (\Exception $e) {
+            Log::error("Failed to attach quest (ID: {$quest->id}) to enrollment (ID: {$enrollment->id})", [
+                'error' => $e->getMessage(),
+                'trace' => $e->getTraceAsString(),
+            ]);
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Failed to assign quest',
+            ], 500);
+        }
 
 
         return response()->json([
