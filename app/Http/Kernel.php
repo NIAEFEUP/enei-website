3db73e6cd05d7eb5f85cd8a8206f13d30b01682e
AvilaAndre <avila_andre14@hotmail.com>
2024-11-24 19:31:16 +0000

    feat: set referral cookie in all routes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
+            \App\Http\Middleware\ReferralCookieSet::class,
         ],
 
         'api' => [
